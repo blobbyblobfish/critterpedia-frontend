99de893e4000c560e06c929ec3c3ec6f2b639e10
blobbyblobfish <aywang6@gmail.com>
2020-07-07 16:03:42 -0400

    Added authentication

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
-function NavBar() {
+function NavBar(props) {
 
     return (
         <div className="navbar">
             <NavLink to="/critters/fish">Fish</NavLink>
             <NavLink to="/critters/bugs">Bugs</NavLink>
             <NavLink to="/critters/sea-creatures">Sea Creatures</NavLink>
-            <NavLink to="/profile">Profile</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/register">Register</NavLink>
+            {props.token
+                ?
+                <React.Fragment>
+                    <NavLink to="/profile">Profile</NavLink>
+                    <NavLink to="/logout">Logout</NavLink>
+                </React.Fragment>
+                :
+                <React.Fragment>
+                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/register">Register</NavLink>
+                </React.Fragment>}
         </div>
     )
 }
